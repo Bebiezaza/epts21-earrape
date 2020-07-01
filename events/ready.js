eba90d5d724caf76a34fc95989f4f57da99847e4
Bebiezaza <Apipunnakul@gmail.com>
2020-07-01 22:45:28 +0700

    'dont force discon me or i will crash'

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = client => {
     client.user.setPresence({
         activity: {
-            name: 'copied code made earrape'
+            name: 'dont force discon me or i will crash'
         }
     });
     console.log('Ready!');

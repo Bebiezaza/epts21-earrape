a9b9b80d459840f4470fc10d2f7361fa0545a4d7
Bebiezaza <Apipunnakul@gmail.com>
2020-09-23 21:06:15 +0700

    Alpha 1.2.3_04

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const prefix = process.env.PREFIX;
 
 module.exports = (client,message,embed) => {
-    embed.setAuthor(client.user.username + " - Alpha 1.2.2", client.user.avatarURL());
+    embed.setAuthor(client.user.username + " - Alpha 1.2.3_04", client.user.avatarURL());
     embed.setColor('#f1c40f');
     embed.setDescription(`__**ALL COMMANDS**__\n
     __**General**__
@@ -12,6 +12,7 @@ module.exports = (client,message,embed) => {
     **${prefix}play** / **${prefix}p** = play/queue songs
     **${prefix}nowplaying** / **${prefix}np** = shows song that is playing in vc
     **${prefix}queue** / **${prefix}q** = shows queue
+    **${prefix}remove [song number according to queue list]** = remove a specific song in the queue
     **${prefix}skip** / **${prefix}s**= skip a song
     **${prefix}disconnect** / **${prefix}dc** = disconnect
     **${prefix}reset** = in case the bot decided to not play any song`);
